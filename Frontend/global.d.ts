93bea8da28cd47b9f9147372f85b39e9684b6ba1
YogeshDhengale <yd30668497@gmail.com>
2024-12-02 20:28:20 +0530

    Fix: Extend global object for mongoose caching in TypeScript

--- Frontend/global.d.ts
+++ Frontend/global.d.ts
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
 declare global {
-  let mongoose: {
-    conn: mongoose.Connection | null;
-    promise: Promise<mongoose.Connection> | null;
-  };
+  namespace NodeJS {
+    interface Global {
+      mongoose: {
+        conn: mongoose.Connection | null;
+        promise: Promise<mongoose.Connection> | null;
+      };
+    }
+  }
 }
+
+export {};
